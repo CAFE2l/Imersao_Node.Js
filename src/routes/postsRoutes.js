f1f8a4cf008ad92cd384c47f2baf963f9fdb8c86
CAFÉ <pciskolargx@gmail.com>
2025-11-20 14:55:19 -0300

    problema

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,24 +1,25 @@
-import express from  "express";
+import express from "express";
 import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
 import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
+    destination: (_req, _file, cb) => {
         cb(null, "./uploads");
     },
-    filename: function(req, file, cb){
-        cb(null, file.originalname);
+    filename: (req, file, cb) => {
+        const timestamp = Date.now();
+        cb(null, `${timestamp}-${file.originalname}`);
     }
 });
 
-const upload = multer({dest:"./uploads", storage})
+const upload = multer({ storage });
 
 const routes = (app) => {
     app.use(express.json());
-    app.get("/posts",listarPosts);
-    app.post("/posts",postarNovoPost);
+    app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
     app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
-export default  routes;
 
+export default routes;
