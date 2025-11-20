8c9e6a235e81147a1f74f8d74ef02e5804e844bd
CAFÉ <pciskolargx@gmail.com>
2025-11-20 14:32:08 -0300

    criando os uploads

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,5 @@
 import { ReturnDocument } from "mongodb";
-import {getTodosPosts, criarPost} from "../Models/postsModel.js";
+import {getTodosPosts, criarPost, uploadImagem} from "../Models/postsModel.js";
 
 export async function listarPosts(req, res){
     const posts = await getTodosPosts();
@@ -9,6 +9,23 @@ export async function listarPosts(req, res){
 
 export async function postarNovoPost(req, res){
     const novoPost = req.body;
+    try{
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+
+   } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({mensagem: "Erro ao criar o post"});
+   }
+}
+
+export async function uploadImagem(req, res){
+    const novoPost = {
+          descricao: "Um gato fazendo panquecas",
+          imgUrl: req.file.originalname,
+          alt: ""
+    };
+
     try{
         const postCriado = await criarPost(novoPost);
         res.status(200).json(postCriado);
