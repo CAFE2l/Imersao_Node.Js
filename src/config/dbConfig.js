625cffdab111c0610caef319e4e6fef90d77383a
CAFÉ <pciskolargx@gmail.com>
2025-11-21 11:33:32 -0300

    todas as corresções nos códigos  devidamente feitas

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,26 +1,17 @@
 import { MongoClient } from 'mongodb';
 
-export default async function conectarAoBanco(stringConexao){
-    let mongoClient;
+export default async function conectarAoBanco(stringConexao) {
+  let mongoClient;
 
-    try{
-        mongoClient = new MongoClient(stringConexao);
-        console.log('Conectando ao cluster do banco de dados...');
-        await mongoClient.connect();
-        console.log('Conectado ao MongoDB Atlas com sucess!');
+  try {
+      mongoClient = new MongoClient(stringConexao);
+      console.log('Conectando ao cluster do banco de dados...');
+      await mongoClient.connect();
+      console.log('Conectado ao MongoDB Atlas com sucesso!');
 
-        return mongoClient;
-    } catch (erro) {
-        console.log("Falha ma conexão com o banco! ", erro);
-        process.exit();
-    }
-}
-
-export async function desconectarDoBanco(client) {
-    try {
-        await client.close();
-        console.log('Desconectado do MongoDB Atlas.');
-    } catch (erro) {
-        console.error('Erro ao desconectar do banco de dados:', erro);
-    }
+      return mongoClient;
+  } catch (erro) {
+      console.error('Falha na conexão com o banco!', erro);
+      process.exit();
+  }
 }
